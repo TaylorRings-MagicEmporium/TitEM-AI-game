754897ad9863411b0e90ec78334f383076cd80c2
Taylor Ring (ringt) <ringt@coventry.ac.uk>
2021-02-17 02:41:58 +0000

    started project, added demo of procedural generational maze concept using off links

---
+++ AI project CW2/Assets/GridState.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class GridState : MonoBehaviour
+{
+
+    public bool NotWalkable = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (NotWalkable)
+        {
+            GetComponent<OffMeshLink>().area = 1;
+        }
+        else if (!NotWalkable)
+        {
+            GetComponent<OffMeshLink>().area = 0;
+        }
+    }
+}
---
+++ AI project CW2/Assets/Player.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Player : MonoBehaviour
+{
+
+    public GameObject target;
+    NavMeshAgent nav;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        nav = GetComponent<NavMeshAgent>();
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        nav.SetDestination(target.transform.position);
+    }
+}
