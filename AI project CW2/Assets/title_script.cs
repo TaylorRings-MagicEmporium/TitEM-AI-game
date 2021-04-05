3f7ea7853fff6a0fe8525ea5303ee8b4499989a5
Taylor Ring (ringt) <ringt@coventry.ac.uk>
2021-04-05 12:59:44 +0100

    added title screen
    also changed walls, floor and light conditions

---
+++ AI project CW2/Assets/title_script.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class title_script : MonoBehaviour
+{
+
+    Animator ani;
+
+    // Update is called once per frame
+    void Update()
+    {
+        ani = GetComponent<Animator>();
+    }
+
+    public void Begin_Game()
+    {
+        StartCoroutine(Game_procedure());
+    }
+
+    IEnumerator Game_procedure()
+    {
+        ani.SetTrigger("continue");
+        yield return new WaitForSeconds(4f);
+        Debug.Log("done!");
+        //SceneManager.LoadScene("game scene");
+    }
+}
