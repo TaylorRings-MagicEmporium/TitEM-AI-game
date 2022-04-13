78dd2198085a04240b1b2f6222cfa55c6d708da0
Taylor Ring (ringt) <64148119+TaylorRings-MagicEmporium@users.noreply.github.com>
2022-04-14 00:43:21 +0100

    added pausing and quitting
    one problem that needs to be fixed is the animation of the invisibility still appears when time = 0. someway of telling the invisibility to hide when needed (Events)

---
+++ AI project CW2/Assets/PauseManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseManager : MonoBehaviour
+{
+    public GameObject PausePanel;
+
+    public bool IsPaused;
+
+    private void Start()
+    {
+        PausePanel.SetActive(false);
+    }
+
+    public void PauseEnabled()
+    {
+        Time.timeScale = 0;
+        PausePanel.SetActive(true);
+    }
+
+    public void PauseDisabled()
+    {
+        Time.timeScale = 1;
+        PausePanel.SetActive(false);
+    }
+
+    public void TogglePause(bool state)
+    {
+        if(IsPaused != state)
+        {
+            IsPaused = state;
+            if (state) PauseEnabled(); else PauseDisabled();
+        }
+    }
+
+}
---
+++ AI project CW2/Assets/TempQuit.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TempQuit : MonoBehaviour
+{
+
+    public void QuitApp()
+    {
+        Debug.Log("QUIT!");
+        Application.Quit();
+    }
+}
